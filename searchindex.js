86d21e0c100d2dd28f40f6bcea5eac49335f054d
ZRY <remyzane@icloud.com>
2016-11-08 11:57:54 +0800

    update

--- searchindex.js
+++ searchindex.js
@@ -1,1 +1,1 @@
-Search.setIndex({envversion:50,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{content:0},titles:["Welcome to Remy&#8217;s Study!"],titleterms:{remi:0,studi:0,welcom:0}})
+Search.setIndex({envversion:50,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{aaa:0,content:0,test:0},titles:["Welcome to Remy&#8217;s Study!"],titleterms:{remi:0,studi:0,welcom:0}})
